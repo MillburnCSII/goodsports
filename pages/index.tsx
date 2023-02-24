c7dba7db845de6cb8b2e552df59332287a68e339
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2023-02-24 15:21:15 -0500

    Update create_post.tsx to follow new data structure and added some styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,4 @@
-import {
-    collection,
-    getDocs,
-    query,
-    limit,
-    Timestamp,
-} from "firebase/firestore";
+import { collection, getDocs, query, limit } from "firebase/firestore";
 import { db } from "../firebaseConfig.js";
 import Nav from "../components/Nav.tsx";
 import Hero from "../components/Hero.jsx";
@@ -52,62 +46,21 @@ interface stupidTs2 {
 }
 
 export default function Home(props) {
-    // const blogData: blogPost[] = [
-    //     {
-    //         id: 1,
-    //         title: "Charity Event - Soccer, Football, Fun and Games!",
-    //         description:
-    //             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero. Incidunt officiis quae voluptates corporis repellendus odio reiciendis, accusantium unde.",
-    //         image: "https://source.unsplash.com/random/?card=4",
-    //         author: "Ester Howard",
-    //         date: "12 Sep 2021",
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "Children We Work With",
-    //         description:
-    //             "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores saepe porro atque obcaecati. Facilis libero et alias quam voluptatum.",
-    //         image: "https://source.unsplash.com/random/?card=5",
-    //         author: "Jacob Jones",
-    //         date: "22 Aug 2021",
-    //     },
-    //     {
-    //         id: 3,
     //         title: "5k Run in the Wind",
-    //         description:
-    //             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur nisi iure, vitae a maxime qui.",
-    //         image: "https://source.unsplash.com/random/?card=6",
     //         author: "Floyd Miles",
-    //         date: "30 Jul 2021",
-    //     },
-    //     {
-    //         id: 4,
+    //         date: "31 Oct 2021",
+
     //         title: "Boston Marathon",
-    //         description:
-    //             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero. Incidunt officiis quae voluptates corporis repellendus odio reiciendis, accusantium unde.",
-    //         image: "https://source.unsplash.com/random/?card=7",
     //         author: "Henry Rollock",
     //         date: "12 Jan 2021",
-    //     },
-    //     {
-    //         id: 5,
+
     //         title: "Boston Legends of the Ball",
-    //         description:
-    //             "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores saepe porro atque obcaecati. Facilis libero et alias quam voluptatum.",
-    //         image: "https://source.unsplash.com/random/?card=8",
     //         author: "Katrina Younce",
     //         date: "20 Aug 2022",
-    //     },
-    //     {
-    //         id: 6,
+
     //         title: "Meet our 2022 New York City Marathon Runners",
-    //         description:
-    //             "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur nisi iure, vitae a maxime qui.",
-    //         image: "https://source.unsplash.com/random/?card=9",
     //         author: "Margaret Norton",
     //         date: "12 Nov 2022",
-    //     },
-    // ];
 
     return (
         <>
@@ -120,12 +73,13 @@ export default function Home(props) {
                 card={BlogCard}
                 data={props.blogData}
             />
-            <CardGroup /* DONATE */
+            {/* DONATE */}
+            {/* <CardGroup 
                 title="Donate Now"
                 subtitle="Be a part of the Cause"
                 card={DonateCard}
                 data={props.donateData}
-            />
+            /> */}
         </>
     );
 }
